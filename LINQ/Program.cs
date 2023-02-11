52b4c8869e7d26638066e4ec86e2c5a1279720d6
Diamond Mccall <singlikediamond@gmail.com>
2023-02-10 18:29:18 -0600

    inital commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace LINQ
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace LINQ
 {
     public class Program
     {
+      
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string> { "Mario bros", "Street Fighter", "Donkey Kong", "2K", "Guitar Hero" };
+
+            var games = videoGames.OrderBy(str => str.Length);
+
+            foreach (var videogames in games)
+            {
+                Console.WriteLine(videogames);
+            }
+
+            Console.WriteLine($"***************");
+
+
+            var letterA = videoGames.Where(letter => letter.Contains("a"));
+            foreach (var letter in letterA)
+            {
+                Console.WriteLine(letter);
+            }
+
+            Console.WriteLine($"***************");
+
+            var name = videoGames.OrderByDescending(str => str.Length) ;
+            foreach (var names in name)
+            {
+                Console.WriteLine(names);
+            }
+
+            Console.WriteLine($"****************");
+
+            var twoK = videoGames.Select(choice => choice.Contains("2"));
+            foreach (var twoks in twoK)
+            {
+                Console.WriteLine(twoks);
+            }
         }
     }
 }
